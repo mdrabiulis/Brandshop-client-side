d34b61b6780018e66c339d569d77a01895fa89ff
mdrabiulis <mdrabiulis155@gmail.com>
2023-10-21 20:53:44 +0600

    Our brand end

--- src/Components/HomeCard/HomeCard.jsx
+++ src/Components/HomeCard/HomeCard.jsx
@@ -14,24 +14,30 @@ const HomeCard = () => {
   }, []);
 
   return (
-    <div className="max-w-[1200px] mx-auto grid md:grid-cols-2 lg:grid-cols-3 gap-8">
-      {cards.map((card) => (
-        <div key={card.id}>
-        <Link to={`/${card.title}`}>
-        <div className="card card-compact bg-base-100 shadow-xl">
-         <figure>
-           <img className="w-80 h-72 rounded-md"
-             src={card.img}
-             alt={card.title}
-           />
-         </figure>
-         <div className="flex justify-center items-center">
-           <h2 className="card-title">{card.title}</h2>
-         </div>
-       </div>
-       </Link>
-     </div>
-      ))}
+    <div className="">
+      <h2 className="text-center text-5xl my-20 font-roboto font-bold">
+        Our brand....
+      </h2>
+      <div className="max-w-[1200px] mx-auto grid md:grid-cols-2 lg:grid-cols-3 gap-8">
+        {cards.map((card) => (
+          <div key={card.id}>
+            <Link to={`/${card.title}`}>
+              <div className="card card-compact bg-base-100 shadow-xl">
+                <figure>
+                  <img
+                    className="w-80 h-72 rounded-md"
+                    src={card.img}
+                    alt={card.title}
+                  />
+                </figure>
+                <div className="flex justify-center items-center">
+                  <h2 className="card-title">{card.title}</h2>
+                </div>
+              </div>
+            </Link>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
